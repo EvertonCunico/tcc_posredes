f63e6c519488c084078065d349e47774476fd52f
Everton Cunico <everton.cunico@gmail.com>
2018-10-18 17:25:01 -0300

    Upload

--- filemgr-mono/src/br/edu/utfpr/filemgr/service/MainService.java
+++ filemgr-mono/src/br/edu/utfpr/filemgr/service/MainService.java
@@ -1,5 +1,11 @@
 package br.edu.utfpr.filemgr.service;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
 import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
@@ -8,6 +14,9 @@ import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+
+import org.glassfish.jersey.media.multipart.FormDataContentDisposition;
+import org.glassfish.jersey.media.multipart.FormDataParam;
 
 import br.edu.utfpr.filemgr.bo.FilemgrBO;
 import io.swagger.annotations.Api;
@@ -25,7 +34,7 @@ public class MainService {
 		this.caminhoPasta = "D:/";
 		this.filemgrbo = new FilemgrBO(this.caminhoPasta);
 	}
-	
+
 	public FilemgrBO getFilemgrbo() {
 		if (filemgrbo == null) {
 			filemgrbo = new FilemgrBO(this.caminhoPasta);
@@ -36,7 +45,7 @@ public class MainService {
 	public void setFilemgrbo(FilemgrBO filemgrbo) {
 		this.filemgrbo = filemgrbo;
 	}
-	
+
 	public String getCaminhoPasta() {
 		return caminhoPasta;
 	}
@@ -62,7 +71,7 @@ public class MainService {
 	public Response download(@PathParam("nomearquivo") String nomearquivo) throws Exception {
 		return this.getFilemgrbo().download(nomearquivo);
 	}
-	
+
 	@POST
 	@Consumes({ MediaType.MULTIPART_FORM_DATA, MediaType.APPLICATION_OCTET_STREAM })
 	@Produces(MediaType.APPLICATION_JSON)
@@ -71,5 +80,45 @@ public class MainService {
 	public Response upload(byte[] database, @PathParam("nomearquivo") String filename, @PathParam("status") String status) {
 		return this.getFilemgrbo().upload(database, filename, status);
 	}
-	
+
+	@POST
+	@Path("/upload")
+	@Consumes(MediaType.MULTIPART_FORM_DATA)
+	public Response uploadFile(
+			@FormDataParam("file") InputStream uploadedInputStream,
+			@FormDataParam("file") FormDataContentDisposition fileDetail) {
+
+		String uploadedFileLocation = "D:/uploaded/" + fileDetail.getFileName();
+
+		// save it
+		writeToFile(uploadedInputStream, uploadedFileLocation);
+
+		String output = "File uploaded to: " + uploadedFileLocation;
+
+		return Response.status(200).entity(output).build();
+
+	}
+
+	private void writeToFile(InputStream uploadedInputStream,
+			String uploadedFileLocation) {
+
+		try {
+			OutputStream out = new FileOutputStream(new File(
+					uploadedFileLocation));
+			int read = 0;
+			byte[] bytes = new byte[1024];
+
+			out = new FileOutputStream(new File(uploadedFileLocation));
+			while ((read = uploadedInputStream.read(bytes)) != -1) {
+				out.write(bytes, 0, read);
+			}
+			out.flush();
+			out.close();
+		} catch (IOException e) {
+
+			e.printStackTrace();
+		}
+
+	}
+
 }
